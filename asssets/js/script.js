29ecff08edef23f1214f0323cd03e1e80dd96675
lokChingt <C24385243@myTUDublin.ie>
2026-04-27 14:11:56 +0100

    feature: add text & slideshow

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -15,3 +15,38 @@ function toggleMenu() {
     }
 
 }
+
+// Slideshow
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+  
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+  
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+  
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+  
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
+  }
